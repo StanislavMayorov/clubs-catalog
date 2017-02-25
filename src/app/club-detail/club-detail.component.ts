b78008531f747de6ac34d330fc9c504af3787cba
StanislavMayorov <Stanislav@fksis.by>
2017-02-25 18:16:42 +0300

    feature: implement club-detail component

--- src/app/club-detail/club-detail.component.ts
+++ src/app/club-detail/club-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from "@angular/router";
 import { FirebaseService } from "../shared/firebase.service";
+import { element } from "protractor";
 
 @Component({
   selector: 'app-club-detail',
@@ -8,16 +9,33 @@ import { FirebaseService } from "../shared/firebase.service";
   styleUrls: ['./club-detail.component.sass']
 })
 export class ClubDetailComponent implements OnInit {
-  clubName: string;
+  clubUniqueId: string;
+  clubs: Array<any>;
+  club: any;
 
-  constructor(private route: ActivatedRoute, private firebaseService: FirebaseService) { }
+  constructor(private route: ActivatedRoute, private firebaseService: FirebaseService) {
+    this.club = null;
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      this.clubName = params['club'];
-      //this.update();
+      this.clubUniqueId = params['club'];
+      this.loadData();
     });
   }
+
+  loadData() {
+    this.firebaseService.getClubs().subscribe(clubs => {
+      let club = Array.from(clubs).filter(element => element.$key === this.clubUniqueId).pop();
+      this.firebaseService.getFile(club.$key, club.fileExtension).then(url => {
+        this.club = {url, name: club.name, description: club.description};
+      }, (error) => {
+        console.error(error);
+      });
+    });
+
+  }
+
 
 }
 
